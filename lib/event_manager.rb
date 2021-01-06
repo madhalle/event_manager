5fd12e788b314dedad3ffeaf16c9d8d84d0564b3
Mad Hallé <mad.halle@icloud.com>
2021-01-05 18:01:44 -0700

    working through event manager

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,5 +1,45 @@
+require 'csv'
+
 puts "EventManager Initialized!"
 
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  full_name = (row[:first_name] + " #{row[:last_name]}")
+  if row[:zipcode] == nil
+    zipcode = "00000"
+  elsif row[:zipcode].size < 5
+    while row[:zipcode].size < 5
+      zipcode = row[:zipcode].prepend("0")
+    end
+  elsif row[:zipcode].size > 5
+    zipcode = row[:zipcode][0..4]
+  else
+    zipcode = row[:zipcode]
+  end
+  zip = zipcode
+  # name = (row[:first_name] + " " + row[:last_name])
+  # puts "Name: #{full_name}, Zip: #{zipcode}"
+  puts full_name.to_s + " " + zip.to_s
+  # puts zipcode
+end
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   full_name = (row[:first_name] + " #{row[:last_name]}")
+#   zipcode = row[:zipcode]
+#   # name = (row[:first_name] + " " + row[:last_name])
+#   # puts "Name: #{full_name}, Zip: #{zipcode}"
+#   puts full_name.to_s + " " + zipcode.to_s
+#   # puts zipcode
+# end
+#
+# contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+# contents.each do |row|
+#   name = (row[:first_name] + " #{row[:last_name]}")
+#   # name = (row[:first_name] + " " + row[:last_name])
+#   puts name
+# end
+
+###pre work###
 # puts File.exist? "event_attendees.csv"
 # contents = File.read "event_attendees.csv"
 # puts contents
@@ -26,19 +66,19 @@ puts "EventManager Initialized!"
 #   puts name
 # end
 
-# row_index = 2
+# row_index = 0
 # lines.each do |line|
 #   row_index += 1
-#   next if row_index == 3
+#   next if row_index == 1
 #   columns = line.split(",")
 #   name = columns[2]
 #   puts name
 # end
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
-  puts name
-end
+# lines = File.readlines "event_attendees.csv"
+# lines.each_with_index do |line, index|
+#   next if index == 0
+#   columns = line.split(",")
+#   name = columns[2]
+#   puts name
+# end
